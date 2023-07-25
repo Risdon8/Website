5e5a7c19e8cfa36859d309070e7e102d0eea0b6f
Risdon8 <110853265+Risdon8@users.noreply.github.com>
2023-07-25 20:44:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function form(number) {
     const form = number + "%";
 }
 form();
+
+function (countdown) {
+    for (let i = 0; i < countdown.length; i++) {
+        console.log(i)
+} 
+}
+countdown(i);
