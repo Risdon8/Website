fe17b56894ee87468ae83097925fb4573c54878b
Risdon8 <110853265+Risdon8@users.noreply.github.com>
2023-07-27 18:31:02 +0100

    summary

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-function form(number) {
-    const form = number + "%";
-}
-form();
-
-function multiply(arr) {
-    const arr[1]*3
-}
-multiply[0, 3, 1];
-
-let sum = 0;
-function addFive(num) {
-    sum = sum + num;
-}
-addFive(5);
-
-
-function (countdown) {
-    for (let i = 0; i < countdown.length; i++) {
-        console.log(i)
-} 
-}
-countdown(i);
+// index.js
+// Your JavaScript code will go here
+// For example, if you want to add functionality to the form, you can do something like this:
+const formList = document.getElementById("username-input");
+// Add your JavaScript logic for the form or other elements here.
