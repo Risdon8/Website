3cc41154ddbbe179d2c9f52c669e36f6830a41a1
Risdon8 <110853265+Risdon8@users.noreply.github.com>
2023-08-03 19:40:43 +0100

    svg

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 // For example, if you want to add functionality to the form, you can do something like this:
 const formList = document.getElementById("username-input");
 // Add your JavaScript logic for the form or other elements here.
+
